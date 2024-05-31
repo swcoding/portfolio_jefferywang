428789c06aee04607e47f82fa87766a5cf63c4c4
Jeffery Wang <sw52099@gmail.com>
2024-05-31 15:59:52 +0800

    Make Hero looks better

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,8 @@ import Link from "next/link";
 import React from "react";
 import {
   AiFillGithub,
-  AiFillInstagram,
   AiFillLinkedin,
   AiFillTwitterCircle,
-  AiFillTwitterSquare,
 } from "react-icons/ai";
 import { DiCssdeck } from "react-icons/di";
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>I'm Jeffery Wang!</SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
