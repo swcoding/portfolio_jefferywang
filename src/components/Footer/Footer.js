c6622ffbe81f1d992b5e16a178a9c6dcc57199ca
Jeffery Wang <sw52099@gmail.com>
2024-05-31 21:54:00 +0800

    make footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import {
+  AiFillGithub,
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+} from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Blog</LinkTitle>
+          <LinkItem href="https://www.hyuanverse.com">hyuanverse.com</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:hyuanverse@gmail.com">
+            hyuanverse@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>
+            Models are like life —— always have room for improvement!
+          </Slogan>
+        </CompanyContainer>
+        <SocialIcons href="https://github.com/swcoding">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/jeffery-wang-712a2b150/">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://twitter.com/firewin15">
+          <AiFillTwitterCircle size="3rem" />
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
