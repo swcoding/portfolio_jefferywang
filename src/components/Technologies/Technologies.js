1c1d6e065ebd3ba8e959a3f1ae2796173d761959
Jeffery Wang <sw52099@gmail.com>
2024-05-31 16:49:09 +0800

    make technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiPython, DiDatabase } from "react-icons/di";
+import { TbSql } from "react-icons/tb";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
+
+const Technologies = () => (
+  <Section>
+    <SectionDivider />
+    <br />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies in the data science world. From
+      Model Building to Data Engineering.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiPython size="3rem" />
+        <ListContainer>
+          <ListTitle>Machine Learning</ListTitle>
+          <ListParagraph>sklearn, XGboost</ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <List>
+      <ListItem>
+        <DiDatabase size="3rem" />
+        <ListContainer>
+          <ListTitle>Data Engineering</ListTitle>
+          <ListParagraph>GCP, Airflow</ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <List>
+      <ListItem>
+        <TbSql size="3rem" />
+        <ListContainer>
+          <ListTitle>Data Analytics</ListTitle>
+          <ListParagraph>Experienced with SQL</ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
